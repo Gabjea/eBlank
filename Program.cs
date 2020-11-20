ec10ac294e529850eb39fb9ff156ea1e703512bf
Gabjea <43420432+Gabjea@users.noreply.github.com>
2020-11-20 22:16:25 +0200

    Revert "main form"

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace eBlank
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Main());
+			Application.Run(new Login());
 		}
 	}
 }
