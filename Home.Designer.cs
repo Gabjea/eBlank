66377716d8399c0512234bcf90789bf075af1799
Gabjea <gabjea856@gmail.com>
2020-11-22 01:33:48 +0200

    home

--- Home.Designer.cs
+++ Home.Designer.cs
@@ -33,10 +33,26 @@ namespace eBlank
 			this.label1 = new System.Windows.Forms.Label();
 			this.timer1 = new System.Windows.Forms.Timer(this.components);
 			this.panel1 = new System.Windows.Forms.Panel();
-			this.panel3 = new System.Windows.Forms.Panel();
 			this.panel2 = new System.Windows.Forms.Panel();
 			this.button1 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.panel5 = new System.Windows.Forms.Panel();
+			this.panel6 = new System.Windows.Forms.Panel();
+			this.panel7 = new System.Windows.Forms.Panel();
+			this.panel8 = new System.Windows.Forms.Panel();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.panel9 = new System.Windows.Forms.Panel();
+			this.panel10 = new System.Windows.Forms.Panel();
+			this.panel11 = new System.Windows.Forms.Panel();
+			this.panel12 = new System.Windows.Forms.Panel();
+			this.panel13 = new System.Windows.Forms.Panel();
+			this.label3 = new System.Windows.Forms.Label();
 			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.panel4.SuspendLayout();
+			this.panel10.SuspendLayout();
+			this.panel11.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -62,18 +78,13 @@ namespace eBlank
 			this.panel1.Size = new System.Drawing.Size(382, 134);
 			this.panel1.TabIndex = 1;
 			// 
-			// panel3
-			// 
-			this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
-			this.panel3.Location = new System.Drawing.Point(663, 354);
-			this.panel3.Name = "panel3";
-			this.panel3.Size = new System.Drawing.Size(300, 250);
-			this.panel3.TabIndex = 3;
-			// 
 			// panel2
 			// 
 			this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.panel2.Controls.Add(this.panel4);
+			this.panel2.Controls.Add(this.label2);
 			this.panel2.Location = new System.Drawing.Point(185, 354);
+			this.panel2.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
 			this.panel2.Name = "panel2";
 			this.panel2.Size = new System.Drawing.Size(300, 250);
 			this.panel2.TabIndex = 4;
@@ -88,13 +99,137 @@ namespace eBlank
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(113, 19);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(81, 13);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "Tomorrow to-do";
+			// 
+			// panel4
+			// 
+			this.panel4.Controls.Add(this.panel8);
+			this.panel4.Controls.Add(this.panel5);
+			this.panel4.Location = new System.Drawing.Point(0, 56);
+			this.panel4.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(300, 194);
+			this.panel4.TabIndex = 1;
+			this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
+			// 
+			// panel5
+			// 
+			this.panel5.Location = new System.Drawing.Point(0, 0);
+			this.panel5.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+			this.panel5.Name = "panel5";
+			this.panel5.Size = new System.Drawing.Size(300, 49);
+			this.panel5.TabIndex = 0;
+			// 
+			// panel6
+			// 
+			this.panel6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.panel6.Location = new System.Drawing.Point(185, 459);
+			this.panel6.Margin = new System.Windows.Forms.Padding(0);
+			this.panel6.Name = "panel6";
+			this.panel6.Size = new System.Drawing.Size(300, 49);
+			this.panel6.TabIndex = 1;
+			// 
+			// panel7
+			// 
+			this.panel7.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.panel7.Location = new System.Drawing.Point(185, 508);
+			this.panel7.Margin = new System.Windows.Forms.Padding(0);
+			this.panel7.Name = "panel7";
+			this.panel7.Size = new System.Drawing.Size(300, 51);
+			this.panel7.TabIndex = 2;
+			// 
+			// panel8
+			// 
+			this.panel8.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.panel8.Location = new System.Drawing.Point(0, 149);
+			this.panel8.Margin = new System.Windows.Forms.Padding(0);
+			this.panel8.Name = "panel8";
+			this.panel8.Size = new System.Drawing.Size(300, 45);
+			this.panel8.TabIndex = 3;
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.panel3.Location = new System.Drawing.Point(666, 459);
+			this.panel3.Margin = new System.Windows.Forms.Padding(0);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(300, 49);
+			this.panel3.TabIndex = 5;
+			// 
+			// panel9
+			// 
+			this.panel9.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.panel9.Location = new System.Drawing.Point(666, 508);
+			this.panel9.Margin = new System.Windows.Forms.Padding(0);
+			this.panel9.Name = "panel9";
+			this.panel9.Size = new System.Drawing.Size(300, 51);
+			this.panel9.TabIndex = 6;
+			// 
+			// panel10
+			// 
+			this.panel10.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.panel10.Controls.Add(this.panel11);
+			this.panel10.Controls.Add(this.label3);
+			this.panel10.Location = new System.Drawing.Point(666, 354);
+			this.panel10.Margin = new System.Windows.Forms.Padding(0);
+			this.panel10.Name = "panel10";
+			this.panel10.Size = new System.Drawing.Size(300, 250);
+			this.panel10.TabIndex = 7;
+			// 
+			// panel11
+			// 
+			this.panel11.Controls.Add(this.panel12);
+			this.panel11.Controls.Add(this.panel13);
+			this.panel11.Location = new System.Drawing.Point(0, 56);
+			this.panel11.Margin = new System.Windows.Forms.Padding(0);
+			this.panel11.Name = "panel11";
+			this.panel11.Size = new System.Drawing.Size(300, 194);
+			this.panel11.TabIndex = 1;
+			// 
+			// panel12
+			// 
+			this.panel12.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.panel12.Location = new System.Drawing.Point(0, 149);
+			this.panel12.Margin = new System.Windows.Forms.Padding(0);
+			this.panel12.Name = "panel12";
+			this.panel12.Size = new System.Drawing.Size(300, 45);
+			this.panel12.TabIndex = 3;
+			// 
+			// panel13
+			// 
+			this.panel13.Location = new System.Drawing.Point(0, 0);
+			this.panel13.Margin = new System.Windows.Forms.Padding(0);
+			this.panel13.Name = "panel13";
+			this.panel13.Size = new System.Drawing.Size(300, 49);
+			this.panel13.TabIndex = 0;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(113, 19);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(80, 13);
+			this.label3.TabIndex = 0;
+			this.label3.Text = "Upcoming tests";
+			// 
 			// Home
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(1140, 726);
-			this.Controls.Add(this.panel2);
 			this.Controls.Add(this.panel3);
+			this.Controls.Add(this.panel6);
+			this.Controls.Add(this.panel9);
+			this.Controls.Add(this.panel7);
+			this.Controls.Add(this.panel10);
+			this.Controls.Add(this.panel2);
 			this.Controls.Add(this.panel1);
 			this.Controls.Add(this.label1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -102,6 +237,12 @@ namespace eBlank
 			this.Text = "Form1";
 			this.Load += new System.EventHandler(this.Home_Load);
 			this.panel1.ResumeLayout(false);
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.panel4.ResumeLayout(false);
+			this.panel10.ResumeLayout(false);
+			this.panel10.PerformLayout();
+			this.panel11.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
@@ -112,8 +253,20 @@ namespace eBlank
 		private System.Windows.Forms.Timer timer1;
 		private System.Windows.Forms.Panel panel1;
 		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.Panel panel3;
 		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.Panel panel5;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Panel panel6;
+		private System.Windows.Forms.Panel panel7;
+		private System.Windows.Forms.Panel panel8;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Panel panel9;
+		private System.Windows.Forms.Panel panel10;
+		private System.Windows.Forms.Panel panel11;
+		private System.Windows.Forms.Panel panel12;
+		private System.Windows.Forms.Panel panel13;
+		private System.Windows.Forms.Label label3;
 	}
 }
 
