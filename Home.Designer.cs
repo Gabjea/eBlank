4a1adc009ef091e5bae55b18f3bb5e35d28527aa
Gabjea <gabjea856@gmail.com>
2020-11-22 00:02:41 +0200

    updates

--- Home.Designer.cs
+++ Home.Designer.cs
@@ -32,33 +32,77 @@ namespace eBlank
 			this.components = new System.ComponentModel.Container();
 			this.label1 = new System.Windows.Forms.Label();
 			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.button1 = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// label1
 			// 
 			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Montserrat Medium", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(340, 275);
+			this.label1.Font = new System.Drawing.Font("Montserrat", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(317, 65);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(0, 29);
+			this.label1.Size = new System.Drawing.Size(520, 33);
 			this.label1.TabIndex = 0;
+			this.label1.Text = "dddddddddddddddddddddddddddddd";
 			// 
 			// timer1
 			// 
 			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Location = new System.Drawing.Point(379, 200);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(382, 134);
+			this.panel1.TabIndex = 1;
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.panel3.Location = new System.Drawing.Point(663, 354);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(300, 250);
+			this.panel3.TabIndex = 3;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.panel2.Location = new System.Drawing.Point(185, 354);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(300, 250);
+			this.panel2.TabIndex = 4;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(150, 92);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// Home
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(1140, 726);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel3);
+			this.Controls.Add(this.panel1);
 			this.Controls.Add(this.label1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.Name = "Home";
 			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Home_Load);
+			this.panel1.ResumeLayout(false);
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
 		}
 
@@ -66,6 +110,10 @@ namespace eBlank
 
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Panel panel2;
 	}
 }
 
--- Home.cs
+++ Home.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,13 +13,18 @@ namespace eBlank
 {
 	public partial class Home : Form
 	{
+		public User user;
+		DB con = new DB();
 
+		string[] ore = new string[9];
 
-		public Home()
+		public string nextclass = "romana";
+
+		public Home(User usr)
 		{
 			InitializeComponent();
 			timer1.Start();
-			//CenterControl(label1);
+			user = usr;
 		}
 
 		private void timer1_Tick(object sender, EventArgs e)
@@ -29,13 +35,91 @@ namespace eBlank
 
 		private void CenterControl(Control c)
 		{
-				
+
 			c.Location = new Point(
 				this.Width / 2 - c.Size.Width / 2,
 				c.Location.Y
 				);
 		}
 
+		private void Home_Load(object sender, EventArgs e)
+		{
+			//CenterControl(label1);
+		}
 
+		private void GetNextClass()
+		{
+			DateTime myDate = DateTime.ParseExact(DateTime.Now.ToString("dd MMMM yyyy").ToString(), "yyyy-MM-dd HH:mm:ss,fff",
+									   System.Globalization.CultureInfo.InvariantCulture);
+
+		}
+
+		private string GetHour()
+		{
+			con.OpenCon();
+			string cmd = "SELECT * FROM ore_cursuri WHERE clasa_id=@clasa_id";
+
+			MySqlCommand command = new MySqlCommand(cmd, con.getCon());
+
+			command.Parameters.AddWithValue("@clasa_id", user.ClasaId);
+
+			using (MySqlDataReader reader = command.ExecuteReader())
+			{
+
+
+
+				if (reader.Read())
+				{
+					for (int i = 0; i < 9; i++)
+						ore[i] = reader[i + 1].ToString();
+
+
+				}
+				
+
+			}
+			 
+			con.CloseCon();
+
+
+
+			return "";
+
+		}
+
+		private string GetClassLink()
+		{
+
+			con.OpenCon();
+			string cmd = "SELECT * FROM materii WHERE clasa_id=@clasa_id";
+
+			MySqlCommand command = new MySqlCommand(cmd, con.getCon());
+
+			command.Parameters.AddWithValue("@clasa_id", user.ClasaId);
+	
+			using (MySqlDataReader reader = command.ExecuteReader())
+			{
+
+
+
+				if (reader.Read())
+				{
+					con.CloseCon();
+					return reader[nextclass].ToString();
+
+
+				}
+				else return "";
+
+			}
+
+
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start(GetClassLink());
+		}
 	}
 }
